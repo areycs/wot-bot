2dad0522470b50e2b7ab5e8fc34b34de231908b2
Alexander Reynolds <wintermute.infinite@gmail.com>
2017-03-07 20:05:58 -0700

    playing with storing post data in an array

--- bot_read.py
+++ bot_read.py
@@ -5,8 +5,16 @@ reddit = praw.Reddit('WOTbot')
 
 subreddit = reddit.subreddit("paranormal")
 
+posts = []
+
 for submission in subreddit.hot(limit=5):
-  print("Title: ", submission.title)
-  print("Text: ", submission.selftext)
-  print("Score: ", submission.score)
-  print("---------------------------------\n")
+  post = [submission.title, submission.selftext]
+
+  posts.append(post)
+
+  # print("Title: ", submission.title)
+  # print("Text: ", submission.selftext)
+  # print("Score: ", submission.score)
+  # print("---------------------------------\n")
+
+print(posts)
